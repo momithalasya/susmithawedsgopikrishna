c746b31eeaaef4aba641e4934316b1c705e7483f
momithalasya <nukalamomithalasya@gmail.com>
2025-09-03 17:54:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const video = document.getElementById('weddingStream');
+const videoSrc = 'https://b32f55b7ad1e.ap-south-1.playback.live-video.net/api/video/v1/ap-south-1.679504361987.channel.bXkd6iPgqmpr.m3u8';
+
+if (Hls.isSupported()) {
+  const hls = new Hls();
+  hls.loadSource(videoSrc);
+  hls.attachMedia(video);
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+  video.src = videoSrc;
+} else {
+  console.error('HLS not supported in this browser.');
+
+}
